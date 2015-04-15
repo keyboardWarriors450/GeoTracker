db6c81ca78ecb36bb43743be8de5d995d6d220d6
Viet <viet@redone>
2015-04-15 11:53:17 -0700

    adding Start and End date , view map pages

--- app/src/main/java/com/mycompany/geotracker/HomeScreen.java
+++ app/src/main/java/com/mycompany/geotracker/HomeScreen.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -18,10 +20,10 @@ public class HomeScreen extends ActionBarActivity {
         final TextView create_user = (TextView)findViewById(R.id.create_user);
 
         create_user.setOnClickListener(new TextView.OnClickListener() {
-            public void onClick(View v) {
-                create_user.setTextColor(Color.parseColor("#67818a"));
-                startActivity(new Intent("geoTracker.RegisterActivity"));
-            }
+                public void onClick(View v) {
+                    create_user.setTextColor(Color.parseColor("#67818a"));
+                    startActivity(new Intent("geoTracker.RegisterActivity"));
+                }
         });
 
         /*create_user.setOnClickListener(new TextView.OnClickListener() {
@@ -30,10 +32,22 @@ public class HomeScreen extends ActionBarActivity {
                 startActivity(new Intent("geoTracker.PasswordRetrieval"));
             }
         });*/
+
+        final Button viewMap = (Button)findViewById(R.id.login);
+
+        viewMap.setOnClickListener(new Button.OnClickListener() {
+
+            public void onClick(View v) {
+                Log.i("test", "HomeScreen");
+                viewMap.setTextColor(Color.parseColor("#67818a"));
+                selectStartEndDate();
+            }
+        });
     }
 
     // Responding to Log in button, it should direct to user input for map view
     public void selectStartEndDate() {
+       // Log.i("test", "HomeScreen");
         Intent intent = new Intent(this, pickDateActivity.class);
        // EditText editText = (EditText) findViewById(R.id.edit_message);
       //  String message = editText.getText().toString();
