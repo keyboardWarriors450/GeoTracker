32346e165085e45ba97b7cc87f0631b43eb83042
David <dyk003@uw.edu>
2015-04-19 04:02:21 -0700

    It now remembers user, and checks if everything is filled out in Registration

--- app/src/main/java/com/mycompany/geotracker/HomeScreen.java
+++ app/src/main/java/com/mycompany/geotracker/HomeScreen.java
@@ -1,67 +1,115 @@
+/*
+ * Copyright (c) 2015. Keyboard Warriors (Alex Hong, Daniel Khieuson, David Kim, Viet Nguyen).
+ * This file is part of GeoTracker.
+ * GeoTracker cannot be copied and/or distributed without the express permission
+ * of Keyboard Warriors.
+ */
+
 package com.mycompany.geotracker;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
+/**
+ * Created by David on April 2015
+ */
 public class HomeScreen extends ActionBarActivity {
+
+    private TextView create_user, forgotPass;
+    private Button login;
+    private EditText user_name, password;
+    private SharedPreferences mPreferences;
+    private String mCurrentUserID, mCurrentPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
 
-        final TextView create_user = (TextView)findViewById(R.id.create_user);
+        mPreferences = getPreferences(Context.MODE_PRIVATE);
+        mCurrentUserID = mPreferences.getString(getString(R.string.curr_userID), "");
+        mCurrentPass = mPreferences.getString(getString(R.string.curr_pass), "");
+        create_user = (TextView) findViewById(R.id.create_user);
+        forgotPass = (TextView) findViewById(R.id.forgot_password);
+        login = (Button) findViewById(R.id.login);
 
         create_user.setOnClickListener(new TextView.OnClickListener() {
-                public void onClick(View v) {
-                    create_user.setTextColor(Color.parseColor("#67818a"));
-                    toRegisterActivity();
-                }
-        });
-
-        /* Viet's page log in listener*/
-        final Button pickDate = (Button)findViewById(R.id.login);
-
-        pickDate.setOnClickListener(new Button.OnClickListener() {
-
+            @Override
             public void onClick(View v) {
-                Log.i("test", "HomeScreen");
-                pickDate.setTextColor(Color.parseColor("#67818a"));
-                toMyAccountActivity();
+                create_user.setTextColor(Color.parseColor("#67818a"));
+                toRegisterActivity();
             }
         });
-         // end log in button listener
 
-        final TextView forgotPass = (TextView)findViewById(R.id.forgot_password);
         forgotPass.setOnClickListener(new TextView.OnClickListener() {
-
+            @Override
             public void onClick(View v) {
                 forgotPass.setTextColor(Color.parseColor("#67818a"));
                 toForgotPassword();
             }
         });
+
+        login.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                user_name = (EditText) findViewById(R.id.user_name);
+                password = (EditText) findViewById(R.id.password);
+                mCurrentUserID = user_name.getText().toString();
+                mCurrentPass = password.getText().toString();
+
+                if (isEmpty(user_name) || isEmpty(password)) {
+                    Toast.makeText(HomeScreen.this, R.string.no_blank, Toast.LENGTH_SHORT).show();
+                } else
+                    toMyAccountActivity();
+            }
+        });
     }
 
-    // Responding to Log in button, it should direct to user input for map view
-    public void toMyAccountActivity() {
-        Intent intent = new Intent(this, MyAccountActivity.class);
-        startActivity(intent);
+    private void toMyAccountActivity() {
+        startActivity(new Intent(this, MyAccountActivity.class));
     }
 
-     /* Viet's page log in listener end here*/
-
-    public void toForgotPassword() {
+    private void toForgotPassword() {
         startActivity(new Intent(this, ForgotPassword.class));
     }
 
-    public void toRegisterActivity() {
+    private void toRegisterActivity() {
         startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    private boolean isEmpty(EditText text) {
+        return text.getText().toString().trim().length() == 0;
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mPreferences = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = mPreferences.edit();
+        editor.putString(getString(R.string.curr_userID), mCurrentUserID);
+        editor.putString(getString(R.string.curr_pass), mCurrentPass);
+        editor.commit();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mPreferences = getPreferences(Context.MODE_PRIVATE);
+        mCurrentUserID = mPreferences.getString(getString(R.string.curr_userID), "");
+        mCurrentPass = mPreferences.getString(getString(R.string.curr_pass), "");
+
+        user_name = (EditText) findViewById(R.id.user_name);
+        password = (EditText) findViewById(R.id.password);
+        user_name.setText(mCurrentUserID);
+        password.setText(mCurrentPass);
     }
 }
--- app/src/main/java/com/mycompany/geotracker/RegisterActivity.java
+++ app/src/main/java/com/mycompany/geotracker/RegisterActivity.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright (c) 2015. Keyboard Warriors (Alex Hong, Daniel Khieuson, David Kim, Viet Nguyen).
+ * This file is part of GeoTracker.
+ * GeoTracker cannot be copied and/or distributed without the express permission
+ * of Keyboard Warriors.
+ */
+
 package com.mycompany.geotracker;
 
 import android.content.Intent;
@@ -7,23 +14,26 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-
+/*
+ * Created by David on April 2015
+ */
 public class RegisterActivity extends ActionBarActivity {
     private String email, password, confirmed_password, question, answer;
     private SharedPreferences preferenceSettings;
     private SharedPreferences.Editor preferenceEditor;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        final Button btn_cancel = (Button)findViewById(R.id.reg_cancel);
-        final Button btn_continue = (Button) findViewById(R.id.reg_continue);
+        Button btn_cancel = (Button)findViewById(R.id.reg_cancel);
+        Button btn_continue = (Button) findViewById(R.id.reg_continue);
 
         btn_cancel.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
@@ -36,17 +46,17 @@ public class RegisterActivity extends ActionBarActivity {
                 preferenceSettings = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
                 preferenceEditor = preferenceSettings.edit();
 
-                TextView reg_email = (TextView)findViewById(R.id.reg_email);
+                EditText reg_email = (EditText)findViewById(R.id.reg_email);
                 email = reg_email.getText().toString();
-                TextView reg_pass = (TextView)findViewById(R.id.reg_password);
+                EditText reg_pass = (EditText)findViewById(R.id.reg_password);
                 password = reg_pass.getText().toString();
-                TextView conf_pass = (TextView)findViewById(R.id.conf_password);
+                EditText conf_pass = (EditText)findViewById(R.id.conf_password);
                 confirmed_password = conf_pass.getText().toString();
-                TextView sec_question = (TextView)findViewById(R.id.sec_question);
+                EditText sec_question = (EditText)findViewById(R.id.sec_question);
                 question = sec_question.getText().toString();
-                TextView sec_answer = (TextView)findViewById(R.id.sec_answer);
+                EditText sec_answer = (EditText)findViewById(R.id.sec_answer);
                 answer = sec_answer.getText().toString();
-
+                CheckBox check = (CheckBox) findViewById(R.id.accept_checkBox);
                 /*
                 Need this to test to see if the password is the same as the confirmed.
                 Wasn't sure what you wanted to put here. For now I made it so that it just
@@ -64,12 +74,24 @@ public class RegisterActivity extends ActionBarActivity {
                 preferenceEditor.putString("answer", answer);
                 preferenceEditor.commit();
 
-                toMyAccountActivity();
+                if (isEmpty(reg_email) || isEmpty(reg_pass) || isEmpty(conf_pass) ||
+                        isEmpty(sec_question) || isEmpty(sec_answer))
+                    Toast.makeText(RegisterActivity.this, R.string.no_blank, Toast.
+                            LENGTH_SHORT).show();
+                else if (!check.isChecked())
+                    Toast.makeText(RegisterActivity.this, R.string.no_check,
+                            Toast.LENGTH_SHORT).show();
+                    else toMyAccountActivity();
             }
         });
     }
 
-    public void toMyAccountActivity() {
+    private void toMyAccountActivity() {
         startActivity(new Intent(this, MyAccountActivity.class));
     }
+
+    private boolean isEmpty(EditText text) {
+        return text.getText().toString().trim().length() == 0;
+    }
+
 }
