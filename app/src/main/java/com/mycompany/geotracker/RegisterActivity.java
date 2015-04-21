186f00725d2c34b459a446d745cabd46407bb8d7
David <dyk003@uw.edu>
2015-04-20 17:58:10 -0700

    Add headers to activity_home_screen and register,  a spinner, check password

--- app/src/main/java/com/mycompany/geotracker/RegisterActivity.java
+++ app/src/main/java/com/mycompany/geotracker/RegisterActivity.java
@@ -13,17 +13,18 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.TextView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 /*
  * Created by David on April 2015
  */
 public class RegisterActivity extends ActionBarActivity {
-    private String email, password, confirmed_password, question, answer;
+    private String email, password, confirmed_password, answer;
     private SharedPreferences preferenceSettings;
     private SharedPreferences.Editor preferenceEditor;
 
@@ -34,6 +35,11 @@ public class RegisterActivity extends ActionBarActivity {
 
         Button btn_cancel = (Button)findViewById(R.id.reg_cancel);
         Button btn_continue = (Button) findViewById(R.id.reg_continue);
+        final Spinner spin_securityQuest = (Spinner) findViewById(R.id.reg_security_quest);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.arr_securityQuest, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin_securityQuest.setAdapter(adapter);
 
         btn_cancel.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
@@ -52,11 +58,11 @@ public class RegisterActivity extends ActionBarActivity {
                 password = reg_pass.getText().toString();
                 EditText conf_pass = (EditText)findViewById(R.id.conf_password);
                 confirmed_password = conf_pass.getText().toString();
-                EditText sec_question = (EditText)findViewById(R.id.sec_question);
-                question = sec_question.getText().toString();
+                String question = (String) spin_securityQuest.getSelectedItem();
                 EditText sec_answer = (EditText)findViewById(R.id.sec_answer);
                 answer = sec_answer.getText().toString();
                 CheckBox check = (CheckBox) findViewById(R.id.accept_checkBox);
+
                 /*
                 Need this to test to see if the password is the same as the confirmed.
                 Wasn't sure what you wanted to put here. For now I made it so that it just
@@ -74,8 +80,12 @@ public class RegisterActivity extends ActionBarActivity {
                 preferenceEditor.putString("answer", answer);
                 preferenceEditor.commit();
 
+                if (!password.equals(confirmed_password)) {
+                    Toast.makeText(RegisterActivity.this, R.string.no_match, Toast.
+                            LENGTH_SHORT).show();
+                } else
                 if (isEmpty(reg_email) || isEmpty(reg_pass) || isEmpty(conf_pass) ||
-                        isEmpty(sec_question) || isEmpty(sec_answer))
+                        isEmpty(sec_answer))
                     Toast.makeText(RegisterActivity.this, R.string.no_blank, Toast.
                             LENGTH_SHORT).show();
                 else if (!check.isChecked())
