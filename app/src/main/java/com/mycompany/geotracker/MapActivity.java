99b1920a1f7bf27110c036e87382cd198c8802c2
Viet <viet@redone>
2015-04-29 11:32:10 -0700

    Add map Api and services

---
+++ app/src/main/java/com/mycompany/geotracker/MapActivity.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2015. Keyboard Warriors (Alex Hong, Daniel Khieuson, David Kim, Viet Nguyen).
+ * This file is part of GeoTracker.
+ * GeoTracker cannot be copied and/or distributed without the express permission
+ * of Keyboard Warriors.
+ */
+
+package com.mycompany.geotracker;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MapActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map);
+    }
+}
