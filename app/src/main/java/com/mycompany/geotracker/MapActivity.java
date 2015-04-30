2f567d6d4688227842291c645d6cad1997120fb5
Viet <viet@redone>
2015-04-30 00:55:40 -0700

    change logo, add my location service, add markers

--- app/src/main/java/com/mycompany/geotracker/MapActivity.java
+++ app/src/main/java/com/mycompany/geotracker/MapActivity.java
@@ -7,19 +7,40 @@
 
 package com.mycompany.geotracker;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v4.app.FragmentActivity;
 
-import android.app.Activity;
-import android.os.Bundle;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends Activity {
+
+public class MapActivity extends FragmentActivity implements OnMapReadyCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_map);
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        map.addMarker(new MarkerOptions()
+                .position(new LatLng(0, -10))
+                .title("Marker"));
+
+        // Sets the map type to be "hybrid" show terrant
+        // map.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+
+        // map.setPadding(0,0,300,0);
+
+        // location
+        map.setMyLocationEnabled(true);
     }
 }
