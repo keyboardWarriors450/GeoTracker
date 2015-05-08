25c9a075d08a60f703d3e3b20d5eafba2406a12f
David <dyk003@uw.edu>
2015-05-08 12:33:47 -0700

    test2

--- app/src/main/java/com/mycompany/geotracker/ViewMapActivity.java
+++ app/src/main/java/com/mycompany/geotracker/ViewMapActivity.java
@@ -27,5 +27,104 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class ViewMapActivity extends ActionBarActivity implements OnMapReadyCallback {
 
     //  private LocationLog mLocationLog;
+    private GoogleMap mGoogleMap;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_map);
+
+        //   mLocationLog = (LocationLog) getIntent().getParcelableExtra("locations");
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.mapView);
+        mGoogleMap = mapFragment.getMap();
+        mapFragment.getMapAsync(this);
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
+        if (mGoogleMap != null) {
+            Log.i("Map Activity", "Inside mGoogleMap");
+            LatLng latlng = new LatLng(47.2528768,-122.4442906 ); // Tacoma coordinates
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(latlng)
+                    .title("Tacoma")
+                    .snippet("This is Tacoma location"));
+
+            LatLng latlng1 = new LatLng(47.2530768,-122.4440906 ); // Tacoma coordinates
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(latlng1)
+                    .title("Tacoma2")
+                    .snippet("This is Tacoma2 location"));
+
+            LatLng latlng2 = new LatLng(47.25387768,-122.4447906 ); // Tacoma coordinates
+            mGoogleMap.addMarker(new MarkerOptions()
+                    .position(latlng2)
+                    .title("Tacoma2")
+                    .snippet("This is Tacoma2 location"));
+            // Move the camera instantly to tacoma with a zoom of 15.
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latlng1, 15));
+        }
+
+        // initial location manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(
+                Context.LOCATION_SERVICE);
+        Location myLocation1 = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        // exacting longitude and latitude from my current location
+        LatLng myLatlng = new LatLng(myLocation1.getLatitude(), myLocation1.getLongitude());
+        if (myLatlng != null) {
+
+            Log.i("Map Activity", "Inside mGoogleMap my current location");
+
+            Marker marker = mGoogleMap.addMarker(new MarkerOptions()
+                    .position(myLatlng)
+                    .title("My Location")
+                    .snippet("This is my current location"));
+
+            // Move the camera instantly to my current location with a zoom of 15.
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(myLatlng, 15));
+        }
+        // Seattle coordinates - 47.6097, -122.3331
+
+      /*  if (mLocationLog != null) {
+
+            List<Location> locations = mLocationLog.getLocationList();
+            Location location = locations.get(0);
+            LatLng firstLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+            for (int i=0; i<locations.size(); i++) {
+                Marker marker = mGoogleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(locations.get(i).getLatitude()
+                                , locations.get(i).getLongitude()))
+                        .title("My Locations"));
+            }
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(firstLatLng, 15));
+        }*/
+
+    }
+
+  /*  @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_location, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
 }
