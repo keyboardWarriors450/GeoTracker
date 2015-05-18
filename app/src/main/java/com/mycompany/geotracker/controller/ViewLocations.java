a918014eec991e776068d199d648fa6cb1101d9f
Viet <viet@redone>
2015-05-17 18:14:16 -0700

    Remodeling User interface and case handling

--- app/src/main/java/com/mycompany/geotracker/controller/ViewLocations.java
+++ app/src/main/java/com/mycompany/geotracker/controller/ViewLocations.java
@@ -8,15 +8,15 @@
  */
 package com.mycompany.geotracker.controller;
 
-import android.app.AlarmManager;
 import android.app.AlertDialog;
-import android.app.PendingIntent;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -27,7 +27,9 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.mycompany.geotracker.receiver.LocationBroadcastReceiver;
@@ -41,6 +43,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 /**
@@ -48,10 +51,10 @@ import java.util.List;
  */
 public class ViewLocations extends ActionBarActivity {
 
-    public final static String START_DATE = "start date";
+    public final static String DATE_TYPE = "DATE TYPE";
     public final static String END_DATE = "end date";
-    private long start;
-    private long end;
+    private static long start;
+    private static long end;
     public final static String LATITUDE = "Latitude";
     private Button mStartButton;
     private Button mStopButton;
@@ -61,30 +64,45 @@ public class ViewLocations extends ActionBarActivity {
     private EditText startTxt;
     private EditText endTxt;
     private DateFormat dfm;
-    private String startStr;
-    private String endStr;
+    private static String startStr;
+    private static String endStr;
     private LocationManager locationManager;
     private LocationListener locationListener;
     private boolean wifiOn = false;
+    private static Button viewMap;
+    private static Button showData;
+    private static TextView startDatePicker;
+    private static TextView endDatePicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pick_date);
-
+        setContentView(R.layout.activity_view_locations);
+        start = 0; end = 0; // initial start end unix time
         locationManager = (LocationManager) this.getSystemService(
                 Context.LOCATION_SERVICE);
         // mLocationList = new ArrayList<>();
 
         // movement data button
-        final Button showData = (Button)findViewById(R.id.show_location);
-
+        showData = (Button)findViewById(R.id.show_location);
+        showData.setEnabled(false);
         showData.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
 
-                showData.setTextColor(Color.parseColor("#67818a"));
                 /** get user id**/
+                MyData myData = new MyData(ViewLocations.this);
+                final ArrayList<User> allData = myData.selectAllUsers();
+
+                String uid = "";
+                if (allData.size() != 0) {
+                    uid = allData.get(allData.size()-1).getUserID();
+                }
+                myData.close();
+
+                new MovementDataFromServer(context).execute(uid, startStr, endStr, " ");
+                /*showData.setTextColor(Color.parseColor("#67818a"));
+                *//** get user id**//*
                 MyData myData = new MyData(ViewLocations.this);
                 final ArrayList<User> allData = myData.selectAllUsers();
                 String uid = "";
@@ -92,7 +110,7 @@ public class ViewLocations extends ActionBarActivity {
                     uid = allData.get(allData.size()-1).getUserID();
                 }
                 myData.close();
-                /* get user input and convert to unix Time Stamp */
+                *//* get user input and convert to unix Time Stamp *//*
                 // start date
                 startTxt = (EditText)findViewById(R.id.startDate);
                 // end date
@@ -125,12 +143,13 @@ public class ViewLocations extends ActionBarActivity {
                     //  toListPoints();  // this bring to list of point toListPoints()
                 } else {
                     Toast.makeText(context, "Invalid inputs or Missing end/start date", Toast.LENGTH_LONG).show();
-                }
+                }*/
             }
         });
 
         // view map from user input location
-        final Button viewMap = (Button)findViewById(R.id.viewMap);
+        viewMap = (Button)findViewById(R.id.viewMap);
+        viewMap.setEnabled(false);
         viewMap.setOnClickListener(new Button.OnClickListener() {
 
             public void onClick(View v) {
@@ -144,15 +163,17 @@ public class ViewLocations extends ActionBarActivity {
                     uid = allData.get(allData.size()-1).getUserID();
                 }
                 myData.close();
+
+                new MovementDataFromServer(context).execute(uid, startStr, endStr, "map");
                 /* get user input and convert to unix Time Stamp */
                 // start date
-                startTxt = (EditText)findViewById(R.id.startDate);
+               // startTxt = (EditText)findViewById(R.id.startDate);
                 // end date
-                endTxt = (EditText)findViewById(R.id.endDate);
+              //  endTxt = (EditText)findViewById(R.id.endDate);
 
-                String startDate = startTxt.getText().toString();
-                String endDate = endTxt.getText().toString();
-                if (isValidDates(startDate,endDate) ) {
+              //  String startDate = startTxt.getText().toString();
+              //  String endDate = endTxt.getText().toString();
+                /*if (isValidDates(startDate,endDate) ) {
 
                     dfm = new SimpleDateFormat("MM-dd-yy");
 
@@ -177,7 +198,7 @@ public class ViewLocations extends ActionBarActivity {
                   //  toViewMap();
                 }  else {
                     Toast.makeText(context, "Invalid inputs or Missing end/start date", Toast.LENGTH_LONG).show();
-                }
+                }*/
             }
         });
 
@@ -262,8 +283,100 @@ public class ViewLocations extends ActionBarActivity {
             public void onProviderDisabled(String provider) {}
         };
 
+        // Select a date from a DatePickerDialog
+        startDatePicker = (TextView) findViewById(R.id.start_date_text);
+        startDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerFragment newFragment = new DatePickerFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString(DATE_TYPE, "start");
+                newFragment.setArguments(bundle);
+                newFragment.show(getFragmentManager(), "datePiker");
+            }
+        });
+
+        endDatePicker = (TextView) findViewById(R.id.end_date_text);
+        endDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                DatePickerFragment newFragment = new DatePickerFragment();
+                Bundle bundle = new Bundle();
+                bundle.putString(DATE_TYPE, " ");
+                newFragment.setArguments(bundle);
+                newFragment.show(getFragmentManager(), "datePiker");
+            }
+        });
     }
 
+    // Date Picker
+    public static class DatePickerFragment extends DialogFragment
+            implements DatePickerDialog.OnDateSetListener {
+
+      //  private String type;
+
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Use the current date as the default date in the picker
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            // Create a new instance of DatePickerDialog and return it
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day) {
+            Bundle bundle = new Bundle();
+            bundle = getArguments();
+            String type = bundle.getString(DATE_TYPE);
+            DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
+            String dateFormat = "";
+            // convert day and month to 2 numbers format
+            if (month < 10 && day < 10) {
+                dateFormat += "0" + (month+1) + "/0" + day + "/" + year;
+            } else if (month < 10) {
+                dateFormat += "0" + (month+1) + "/" + day + "/" + year;
+            } else if (day < 10) {
+                dateFormat += (month + 1) + "/0" + day + "/" + year;
+            }else { dateFormat += (month + 1) + "/" + day + "/" + year;}
+
+            String startD = "";
+            String endD = "";
+
+
+            if (type.equals("start")) {
+                startDatePicker.setText(" " + dateFormat + " ");
+                startD = dateFormat;
+                try {
+                    start = df.parse(startD).getTime() / 1000; // start date Unix time
+                    startStr = Long.toString(start);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+            } else {
+                endDatePicker.setText(" " + dateFormat + " ");
+                endD = dateFormat;
+                try {
+                    end = df.parse(endD).getTime() / 1000; // end date Unix Time
+                    if (end > start) { viewMap.setEnabled(true); showData.setEnabled(true);}
+
+                    else { Toast.makeText(view.getContext(), "End Date must greater than Start Date "
+                            , Toast.LENGTH_SHORT).show();}
+                    endStr = Long.toString(end);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            Toast.makeText(view.getContext(), "You selected " + dateFormat , Toast.LENGTH_SHORT).show();
+
+        }
+    }
 
     /**
      * Validates user inputs.
